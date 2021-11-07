2d47cfd052d2eb1f0e68783080155afbda43776f
narumi763 <nk.web.div@gmail.com>
2021-11-07 09:38:44 +0900

    いったんシンプルに

--- walking_map/lib/main.dart
+++ walking_map/lib/main.dart
@@ -16,7 +16,6 @@ class MapApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Walking Map'),
@@ -41,85 +40,8 @@ class StaticMapView extends StatelessWidget{
   static const _imageUrl =
       '$_baseMapUrl?$_mapCenter&$_mapZoom&$_mapMarkers&$_mapSize&$_scale&$_language&$_apiKey';
   
-  @override
-  Widget build(BuildContext context){
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child:  Center(
-        child:  Column(
-          children: <Widget>[
-            const SizedBox(height: 240,),
-            Image.network(_imageUrl, fit:BoxFit.cover,),
-            const SizedBox(height: 240,)
-          ],
-        ),
-      ),
-    );
-  }
+   @override
+   Widget build(BuildContext context){
+     return Image.network(_imageUrl);
+   }
 }
-
-// class MapApp extends StatefulWidget {
-//   @override
-//   _MapAppState createState() => new _MapAppState();
-// }
-
-// class _MapAppState extends State<MapApp>{
-//   String _title = '地図開拓';
-//   Widget build(BuildContext context){
-//     return MaterialApp(
-//       title:  'WalkingMap',
-//       theme: ThemeData(
-//         primarySwatch:  Colors.blue,
-//       ),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text(_title),
-//           actions: <Widget>[
-//             IconButton(
-//               onPressed: (){},
-//               icon: Icon(Icons.settings),
-//             )
-//           ],
-//         ),
-//         body: FlutterMap(
-//           options: MapOptions(
-//             center: LatLng(35.681, 139.767),
-//             zoom: 14.0,
-//           ),
-//           layers: [
-//             TileLayerOptions(
-//               urlTemplate: 
-//                  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-//               subdomains: ['a', 'b', 'c']
-//             ),
-//             CircleLayerOptions(
-//               circles: [
-//                 CircleMarker(
-//                   color: Colors.indigo.withOpacity(0.9),
-//                   radius: 10,
-//                   borderColor: Colors.white.withOpacity(0.9),
-//                   borderStrokeWidth: 3,
-//                   point: LatLng(35.681, 139.760),
-//                 ),
-//                 CircleMarker(
-//                   color:  Colors.teal.withOpacity(0.5),
-//                   radius: 15,
-//                   borderColor: Colors.white.withOpacity(0.5),
-//                   borderStrokeWidth: 5,
-//                   point: LatLng(35.675, 139.770),
-//                 ),
-//                 CircleMarker(
-//                   color:  Colors.yellow.withOpacity(0.7),
-//                   radius: 20,
-//                   borderColor: Colors.white.withOpacity(0.7),
-//                   borderStrokeWidth: 6,
-//                   point: LatLng(35.687, 139.775),
-//                 ),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
